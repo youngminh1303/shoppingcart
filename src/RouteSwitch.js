47faa61f3f438c277839aec5953554b8273d3f41
youngminh1303 <quangminh13032003@gmail.com>
2022-06-10 17:41:18 +0700

    a

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -11,9 +11,9 @@ const RouteSwitch = () => {
   return (
       <BrowserRouter>
           <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/shop" element={<Shop counter={counter} setCounter={setCounter} setItems={setItems} total={total} setTotal={setTotal} />}></Route>
-        <Route path="/cart" element={<Cart counter={counter} setCounter={setCounter} items={items} total={total} setTotal={setTotal} setItems={setItems}/>}></Route>
+        <Route path="/shoppingcart" element={<App />}></Route>
+        <Route path="/shoppingcart/shop" element={<Shop counter={counter} setCounter={setCounter} setItems={setItems} total={total} setTotal={setTotal} />}></Route>
+        <Route path="/shoppingcart/cart" element={<Cart counter={counter} setCounter={setCounter} items={items} total={total} setTotal={setTotal} setItems={setItems}/>}></Route>
         </Routes>
       </BrowserRouter>
   )
